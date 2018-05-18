90e6f81ef66a2f00491c82e53c73d5e93f29ae9a
Daniel Wood <daniel.j.wood11@gmail.com>
2018-05-18 15:47:40 -0400

    add lines and stuff

--- js/script.js
+++ js/script.js
@@ -7,22 +7,8 @@ d3.queue()
     .await(ready);
 
 function ready(error, data) {    
-	// console.log(data)
-
-	// mess with the data as needed
-	// remove too small data?
-
-	// https://bl.ocks.org/officeofjane/7e70dc106a6f4c0e5163f994a5f1c219
-
-	// build the chart margins and container in d3
-
-	var test = [
-	{value:"Switzerland",data:"test"},
-	{value:"Azerbaijan",data:"test"},	
-	{value:"Madagascar",data:"test"}];
-
 	// make data searchable in autocomplete by adding the value category. 
-	data.forEach(function(d){		
+	data.forEach(function(d){
 		d.value = d.CountyName + " County, " + d.State;
 	})
 
@@ -36,64 +22,43 @@ function ready(error, data) {
 	      return "No county found"
 	    },
 	    onSelect: function ( suggestion ) {
-	      console.log(suggestion)
+	      // console.log(suggestion)
 	      // CAll function that highlights selected line
 	      // Highlight( suggestion.value )
 	    }
 	  } );
 
 	// event for clicking on buttons to change data
-	$('input[type="button"]').on('click',function(){  	
-		$('input[type="button"]').removeClass("active");	
-		update(data,this.name)
+	$('.switch.dots.second-in').on('click',function(){		
+		$('.switch.dots.second-in').removeClass("active");
+		update(data,$(this)["0"].attributes[2].nodeValue,y)
 		$(this).addClass("active");
-	});
+	})
 
 	
 	var breakpoint = 768,
-		width = parseInt(d3.select("#chart").style("width")) * 3;
+		width = parseInt(d3.select("#chart").style("width"));
 
 	// static parameters
 	var spacer = 10,
-		bubbleRadius = 5;
+		bubbleRadius = 4,
+		bnMult = bubbleRadius*2+5,
+		topBubbleLevel = -8
 
 	// width based parameters
 	if (width > breakpoint) {
-		var margin = {top: 10, right: 10, bottom: 20, left: 130},
-			height = 1000,
-			gapBetweenGroups = 20,
-			spaceForLabelsLeft   = 80,
-			spaceForLabelsRight = 30
+		var margin = {top: 20, right: 10, bottom: 10, left: 100}
 	} else {
 		// Smaller viewports
-		var margin = {top: 10, right: 10, bottom: 10, left: 30},
-			height = 500,			
-			spaceForLabelsLeft   = 30,
-			spaceForLabelsRight = 30
-	}
-
-	// var chartHeight = data.length * (bubbleRadius*2);
-	var chartHeight = 200 * (bubbleRadius*2);
+		var margin = {top: 20, right: 10, bottom: 10, left: 10}
+	}	
 
 	var colorScale = d3.scaleOrdinal()
 		.range(["#848081","#d5d5d4","#332d2f","#5c5859","#0096d2","#a2d4ec","#0a4c6a","#12719e","#fdbf11","#fce39e","#843215","#e88e2d"]);		
 
-	
-  	 
-	var chartWidth = (width/3) - margin.left-margin.right;
-
-	var svg = d3.select("#chart").append("svg")
-	  .attr("width", width)
-	  .attr("height", chartHeight + margin.top + margin.bottom);
-
-  	// declare main g and stuff
-  	var g = svg.append("g").attr("transform", "translate(" + margin.left + "," + 0 + ")");
-
   	//////// mess with the data
 
   	var y = {}
-
-	console.log(data)
 
 	// create d3 key here. 
 	var map = d3.map(data, function(d) { return d.id; });
@@ -101,14 +66,31 @@ function ready(error, data) {
 	for (var k = 0; k < indicators.length; k++) {	
 		// var extent = d3.extent(data, function(d) { return +d[indicators[k]]});
 		var min = Math.floor(d3.min(data, function(d) { return +d[indicators[k]]}));
-		var max = Math.ceil(d3.max(data, function(d) { return +d[indicators[k]]}));
-		
+		// var max = Math.ceil(d3.max(data, function(d) { return +d[indicators[k]]}));
+		var max = 3;		
+		// if (indicators[k] === "z_Business") {
+		// 	max = 3;
+		// }
+		// else if (indicators[k] === "z_CommDevOther") {
+		// 	max = 3; 
+		// }
+		// else if (indicators[k] === "z_GlobalCapacity") {
+		// 	max = 3;
+		// }
+		// else if (indicators[k] === "z_Housing") {
+		// 	max = 3;
+		// }
+		// else if (indicators[k] === "z_ImpactFinance") {
+		// 	max = 3;
+		// }
+
+
 		// Problem here!!!! 
 		// Can only have (width/radius) balls per row. 
 
 		var histogram = d3.histogram()
 			.domain([min,max])
-			.thresholds(109)
+			.thresholds(100)
 			.value(function(d,i,data) { 
 				return +d[indicators[k]]; 
 			})
@@ -117,7 +99,9 @@ function ready(error, data) {
 
 		y[indicators[k]] = d3.scaleLinear()
 			.domain([min,max])
-			.range([chartWidth-(spaceForLabelsLeft+spaceForLabelsRight),0]); 
+			.range([bins.length*bnMult,0]); 
+
+		y[indicators[k]].numBins = bins.length;
 
 		for (var i = 0; i < bins.length; i++) {
 			// bins[i].sort(function(a,b){return b[indicators[k]] - a[indicators[k]]  })
@@ -125,54 +109,126 @@ function ready(error, data) {
 				var now = map.get(bins[i][j].id);
 				now[indicators[k] + "Index"] = j;
 				now[indicators[k] + "Y"] = (bins[i].x1 + bins[i].x0)/2;
-				
 			}
 		}
 	}
+
+	var chartHeight = 200 * (bubbleRadius*2);
+	var svg = d3.select("#chart").append("svg")
+	  .attr("width", width)
+	  .attr("height", chartHeight + margin.top + margin.bottom);
+
+  	// declare main g and stuff
+  	var g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+  function update(data,indicator,y) {
+  	var t = d3.transition()
+      .duration(1500);
+
+	// Index of county bubbles over the max height
+    var overIndex = -1;
+    // reset height and redraw based on width?
+    
+	
+	var chartHeight = y[indicator].numBins*(bnMult);
+
+	svg.attr("height",chartHeight+margin.top+margin.bottom)
+
+	//remove existing yGrid lines	
+	g.selectAll(".yGrid").remove()		
+	g.selectAll(".yNum").remove()		
+
+	for (var i = y[indicator].domain()[0]; i <= y[indicator].domain()[1]; i++) {
+		console.log(i)
+		g.append("line")
+			.attr("class","yGrid")
+			.attr("x1",function(d){
+				if (i === 0) {
+					return -75
+				} else {
+					return -40
+				}
+			})
+			.attr("x2",width)
+			.attr("y1",y[indicator](i))
+			.attr("y2",y[indicator](i))
+
+		g.append("text")
+			.attr("class","yNum")
+			.attr("x",function(d){
+				if (i === 0) {
+					return -70
+				} else {
+					return -35
+				}
+			})			
+			.attr("y",y[indicator](i) - 5)
+			.text(function(){
+				if (i === y[indicator].domain()[1]) {
+					return i + "+"
+				} else {
+					return i
+				}
+			})			
+	}
 	
 
-  function update(data,indicator) {
-  	var t = d3.transition()
-      .duration(750);
-
-    console.log(y)
-
   	// data join
-	  var counties = g.selectAll("circle")
-	  	.data(data, function(d) {return d.fips5; });
+	var counties = g.selectAll("circle")
+		.data(data, function(d) {return d.fips5; });
 
-	 	// EXIT old elements not present in new data.
-	  counties.exit()
-	      .attr("class", "exit")
-	    .transition(t)
-	      .style("fill-opacity", 1e-6)
-	      .remove();
+		// EXIT old elements not present in new data.
+	counties.exit()
+	  .attr("class", "exit")
+	.transition(t)
+	  .style("fill-opacity", 1e-6)
+	  .remove();
 
 	  // update
   	counties.attr("class", "update")
       .style("fill-opacity", 1)
     	.transition(t)
 			.attr("cx", function (d) { 
-				return d[indicator + "Index"]*bubbleRadius*2.5;
+				if (isNaN(d[indicator + "Y"])) {
+					overIndex += 1;
+					return overIndex*bnMult;
+				}
+				else {
+					return d[indicator + "Index"]*bubbleRadius*3;	
+				}				
 			})
-			.attr("cy", function (d,i) { 					
-				return y[indicator](d[indicator + "Y"])
-				// return y["z_GlobalCapacity"](d.z_GlobalCapacity);
+			.attr("cy", function (d) { 	
+				if (isNaN(d[indicator + "Y"])) {
+					return topBubbleLevel;
+				}	
+				else {
+					return y[indicator](d[indicator + "Y"])	
+				}	
 			})
 
 	  // enter + update
 	  counties.enter().append("circle")
       	.attr("class", "enter")
 			.attr("cx", function (d) { 
-				return d[indicator + "Index"]*bubbleRadius*2.5;
+				if (isNaN(d[indicator + "Y"])) {
+					overIndex += 1;
+					return overIndex*bnMult;
+				}
+				else {
+					return d[indicator + "Index"]*bubbleRadius*3;	
+				}		
 			})
-			.attr("cy", function (d,i) { 					
-				return y[indicator](d[indicator + "Y"])
-				// return y["z_GlobalCapacity"](d.z_GlobalCapacity);
+			.attr("cy", function (d) {
+				if (isNaN(d[indicator + "Y"])) {
+					return topBubbleLevel;
+				}	
+				else {
+					return y[indicator](d[indicator + "Y"])	
+				}	
 			})
 			.attr("r", bubbleRadius)
 			.style("fill", function(d) {
-				return "#ff00ff"
+				return "#1696D2"
 			})
 			.style("fill-opacity", 1e-6)
     	.transition(t)
@@ -180,8 +236,9 @@ function ready(error, data) {
 
   }
 
-  var indicator = "z_GlobalCapacity"
-  update(data,indicator)
+  // starting indicator?
+  var indicator = "z_Housing"
+  update(data,indicator,y)
 
 	// Grab a random sample of letters from the alphabet, in alphabetical order.
 	// d3.interval(function() {
